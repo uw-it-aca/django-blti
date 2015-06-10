8cf2ffa4eb0d77f482f95fd4b395a0d385a09b79
mike seibel <mikes@washington.edu>
2015-06-10 15:54:44 -0700

    rest dispatch auth overridable

--- blti/views/rest_dispatch.py
+++ blti/views/rest_dispatch.py
@@ -6,33 +6,44 @@ import json
 import re
 
 
+class RESTDispatchAuthorization(Exception): pass
+class RESTDispatchMethod(Exception): pass
+
+
 class RESTDispatch(object):
     """ A superclass for views, that handles passing on the request to the
         appropriate view method, based on the request method.
     """
     @never_cache
     def run(self, *args, **named_args):
-        request = args[0]
+        try:
+            request = args[0]
+            self.authorize(request)
+            return self.dispatch(request.method)(*args, **named_args)
+        except RESTDispatchAuthorization as ex:
+            return self.error_response(401, "%s" % ex)
+        except RESTDispatchMethod:
+            return self.invalid_method_response(*args, **named_args)
 
+    def authorize(self, request):
+        self.blti_authorize(request)
+
+    def blti_authorize(self, request):
         try:
             BLTI().get_session(request)
         except BLTIException as ex:
-            if (not getattr(settings, 'BLTI_NO_AUTH', False) and
-                    not request.user.is_authenticated()):
-                return self.error_response(401, "%s" % ex)
+            if not (getattr(settings, 'BLTI_NO_AUTH', False) and
+                    request.user.is_authenticated()):
+                raise RESTDispatchAuthorization('%s' % ex)
 
-        return self.run_http_method(*args, **named_args)
+    def dispatch(self, method):
+        methods = dict((m,m) for m in ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'PATCH'])
+        try:
+            return getattr(self, methods[method])
+        except (KeyError, AttributeError):
+            raise RESTDispatchMethod()
 
-    def run_http_method(self, *args, **named_args):
-        request = args[0]
-
-        if (re.match(r'(GET|HEAD|POST|PUT|DELETE|PATCH)', request.method) and
-                hasattr(self, request.method)):
-            return getattr(self, request.method)(*args, **named_args)
-        else:
-            return self.invalid_method(*args, **named_args)
-
-    def invalid_method(self, *args, **named_args):
+    def invalid_method_response(self, *args, **named_args):
         return HttpResponse('Method not allowed', status=405)
 
     def error_response(self, status, message='', content={}):
