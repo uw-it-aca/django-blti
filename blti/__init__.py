ae93f6a935908c957136ca0b59541b5ab09bb6fe
Jim Laney <jlaney@uw.edu>
2016-04-27 13:10:41 -0700

    moves oauth validation out of __init__

--- blti/__init__.py
+++ blti/__init__.py
@@ -1,11 +1,10 @@
-import time
 import json
 import urllib
-from oauth import oauth
 from base64 import b64decode, b64encode
 from django.conf import settings
 from blti.crypto import aes128cbc
-from blti.models import BLTIKeyStore
+from blti.oauth import validate as oauth_validate
+from oauth import OAuthError
 import re
 
 
@@ -64,35 +63,15 @@ class BLTI(object):
         else:
             raise BLTIException('Missing or malformed parameter or value')
 
-        launch = self.oauth_validate(request, params=params)
+        try:
+            launch = oauth_validate(request, params=params)
+        except oauth.OAuthError as err:
+            raise BLTIException(err)
+
         if visibility:
             self.visibility_validate(launch, visibility)
 
         return launch
-
-    def oauth_validate(self, request, params={}):
-        try:
-            self._oauth_server = oauth.OAuthServer(data_store=BLTIDataStore())
-            self._oauth_server.add_signature_method(
-                oauth.OAuthSignatureMethod_HMAC_SHA1())
-
-            oauth_request = oauth.OAuthRequest.from_request(
-                request.method,
-                request.build_absolute_uri(),
-                headers=request.META,
-                parameters=params
-            )
-
-            if oauth_request:
-                consumer = self._oauth_server._get_consumer(oauth_request)
-                self._oauth_server._check_signature(oauth_request,
-                                                    consumer, None)
-                return oauth_request.get_nonoauth_parameters()
-
-            raise BLTIException('Invalid OAuth Request')
-
-        except oauth.OAuthError as err:
-            raise BLTIException('%s' % err)
 
     def visibility_validate(self, params, visibility):
         if visibility:
@@ -157,50 +136,3 @@ class BLTI(object):
     def decrypt_session(self, string):
         aes = aes128cbc(settings.BLTI_AES_KEY, settings.BLTI_AES_IV)
         return json.loads(aes.unpad(aes.decrypt(b64decode(string))))
-
-
-class BLTIDataStore(oauth.OAuthDataStore):
-    def lookup_consumer(self, key):
-        try:
-            model = BLTIKeyStore.objects.get(consumer_key=key)
-            return BLTIConsumer(key, model.shared_secret)
-
-        except BLTIKeyStore.DoesNotExist:
-            try:
-                consumers = getattr(settings, 'LTI_CONSUMERS', {})
-                return BLTIConsumer(key, consumers[key])
-            except KeyError:
-                return None
-
-    def lookup_nonce(self, oauth_consumer, oauth_token, nonce):
-        return nonce if oauth_consumer.CheckNonce(nonce) else None
-
-
-class BLTIConsumer(oauth.OAuthConsumer):
-    """
-    OAuthConsumer superclass that adds nonce caching
-    """
-    def __init__(self, key, secret):
-        oauth.OAuthConsumer.__init__(self, key, secret)
-        self.nonces = []
-
-    def CheckNonce(self, nonce):
-        """
-        Returns True if the nonce has been checked in the last hour
-        """
-        now = time.time()
-        old = now - 3600.0
-        trim = 0
-        for n, t in self.nonces:
-            if t < old:
-                trim = trim + 1
-            else:
-                break
-        if trim:
-            self.nonces = self.nonces[trim:]
-
-        for n, t in self.nonces:
-            if n == nonce:
-                return True
-
-        self.nonces.append((nonce, now))
