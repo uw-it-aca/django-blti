515b44ab4cbd6ff01c358ca769e4f3a9b23e1c30
Jim Laney <jlaney@uw.edu>
2017-01-30 14:52:34 -0800

    drops custom exception

--- blti/crypto.py
+++ blti/crypto.py
@@ -2,10 +2,6 @@ from Crypto.Cipher import AES
 from base64 import b64decode, b64encode
 
 
-class CryptoException(Exception):
-    pass
-
-
 class aes128cbc(object):
 
     _key = None
@@ -14,36 +10,28 @@ class aes128cbc(object):
     def __init__(self, key, iv):
         """
         Advanced Encryption Standard object
-
-        Raises CryptoException
         """
         self._bs = 16  # Block size
 
         if key is None:
-            raise CryptoException('Missing AES key')
+            raise ValueError('Missing AES key')
         else:
             self._key = key
 
         if iv is None:
-            raise CryptoException('Missing AES initialization vector')
+            raise ValueError('Missing AES initialization vector')
         else:
             self._iv = iv
 
     def encrypt(self, msg):
-        try:
-            msg = self._pad(self.str_to_bytes(msg))
-            crypt = AES.new(self._key, AES.MODE_CBC, self._iv)
-            return b64encode(self._iv + crypt.encrypt(msg)).decode('utf-8')
-        except Exception as err:
-            raise CryptoException('Cannot encrypt message: %s' % err)
+        msg = self._pad(self.str_to_bytes(msg))
+        crypt = AES.new(self._key, AES.MODE_CBC, self._iv)
+        return b64encode(self._iv + crypt.encrypt(msg)).decode('utf-8')
 
     def decrypt(self, msg):
-        try:
-            msg = b64decode(msg)
-            crypt = AES.new(self._key, AES.MODE_CBC, self._iv)
-            return self._unpad(crypt.decrypt(msg[self._bs:])).decode('utf-8')
-        except Exception as err:
-            raise CryptoException('Cannot decrypt message: %s' % err)
+        msg = b64decode(msg)
+        crypt = AES.new(self._key, AES.MODE_CBC, self._iv)
+        return self._unpad(crypt.decrypt(msg[self._bs:])).decode('utf-8')
 
     def _pad(self, s):
         return s + (self._bs - len(s) % self._bs) * self.str_to_bytes(chr(
