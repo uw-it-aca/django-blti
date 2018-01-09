27f0343bdd581465ad7047ba98ac7227fb380dd1
Jim Laney <jlaney@uw.edu>
2018-01-09 11:18:39 -0800

    adds a new rest dispatch based on BLTIView class

--- blti/views/__init__.py
+++ blti/views/__init__.py
@@ -1,8 +1,11 @@
+from django.http import HttpResponse
 from django.views.generic.base import TemplateView
 from django.views.decorators.csrf import csrf_exempt
 from blti import BLTI, BLTIException
 from blti.models import BLTIData
 from blti.validators import BLTIOauth, BLTIRoles
+from blti.performance import log_response_time
+import json
 try:
     from urllib import unquote_plus
 except ImportError:
@@ -72,3 +75,33 @@ class BLTILaunchView(BLTIView):
 class RawBLTIView(BLTILaunchView):
     template_name = 'blti/raw.html'
     authorized_role = 'admin'
+
+
+class RESTDispatch(BLTIView):
+    """
+    A superclass for API views
+    """
+    authorized_role = 'member'
+
+    @log_response_time
+    def dispatch(self, request, *args, **kwargs):
+        try:
+            kwargs['blti_params'] = self.validate(request)
+        except BLTIException as ex:
+            return self.error_response(401, ex)
+
+        return super(BLTIView, self).dispatch(request, *args, **kwargs)
+
+    def render_to_response(self, context, **kwargs):
+        return self.json_response(content=context)
+
+    def error_response(self, status, message='', content={}):
+        content['error'] = str(message)
+        return self.json_response(content=content, status=status)
+
+    def json_response(self, content={}, status=200):
+        response = HttpResponse(json.dumps(content),
+                                status=status,
+                                content_type='application/json')
+        self.add_headers(response=response, **kwargs)
+        return response
