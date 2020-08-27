daf6236c16efef7ec99fa2c7cf32c4e4be36f12c
mike seibel <mikes@washington.edu>
2020-08-27 09:11:59 -0700

    adds development lti launch

--- blti/urls.py
+++ blti/urls.py
@@ -1,7 +1,16 @@
-from django.conf.urls import url
+from django.conf import settings
+from django.urls import re_path
 from blti.views import RawBLTIView
 
 
 urlpatterns = [
-    url(r'^$', RawBLTIView.as_view()),
+    re_path(r'^$', RawBLTIView.as_view()),
 ]
+
+if (getattr(settings, 'LTI_DEVELOP_APP', None)
+        and getattr(settings, "DEBUG", False)):
+    from blti.views.develop import BLTIDevPrepare, BLTIDevLaunch
+    urlpatterns += [
+        re_path(r'dev[/]?$', BLTIDevPrepare.as_view()),
+        re_path(r'dev/launch[/]?', BLTIDevLaunch.as_view()),
+    ]
