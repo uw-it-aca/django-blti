b8e03ada07d3d006294a1c6bf30cb2f9263d5d69
mike seibel <mikes@washington.edu>
2024-12-03 17:36:00 -0800

    license text

--- blti/tests/settings.py
+++ blti/tests/settings.py
@@ -1,3 +1,6 @@
+# Copyright 2024 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
 SECRET_KEY = 'fake_key'
 DEBUG = True
 DATABASES = {
--- blti/tests/urls.py
+++ blti/tests/urls.py
@@ -1,3 +1,6 @@
+# Copyright 2024 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
 from django.conf.urls import include
 from django.urls import re_path
 
