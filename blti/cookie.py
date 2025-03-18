e7e71994ae1e9a646b487b162d711efc90e35a8a
mike seibel <mikes@washington.edu>
2025-03-18 12:04:55 -0700

    cookie smoke

--- blti/cookie.py
+++ blti/cookie.py
@@ -17,47 +17,23 @@ if "partitioned" not in Cookie.Morsel._reserved:
 
 
 class BLTICookieService(DjangoCookieService):
+    def __init__(self, request):
+        return super().__init__(request)
+
     def get_cookie(self, name):
         logger.info(f"get_cookie: {name}")
-        return self._request.get_cookie(self._get_key(name))
+        return super().get_cookie(name)
 
     def set_cookie(self, name, value, exp=3600):
         logger.info(f"set_cookie: {name}: {value}")
-        self._cookie_data_to_set[self._get_key(name)] = {
-            "value": value,
-            "exp": exp,
-        }
+        return super().set_cookie(name, value, exp)
 
     def update_response(self, response):
-        logger.info(f"update_response: {self._cookie_data_to_set}")
+        super().update_response(response)
+
         for key, cookie_data in self._cookie_data_to_set.items():
-            kwargs = {
-                "value": cookie_data["value"],
-                "max_age": cookie_data["exp"],
-                "secure": self._request.is_secure(),
-                "httponly": True,
-                "path": "/",
-            }
-
             if self._request.is_secure():
-                # samesite argument was added in Django 2.1, but samesite could be set as None only from Django 3.1
-                # https://github.com/django/django/pull/11894
-                django_support_samesite_none = django.VERSION[0] > 3 or (
-                    django.VERSION[0] == 3 and django.VERSION[1] >= 1
-                )
-
-                # SameSite=None and Secure=True are required to work inside iframes
-                if django_support_samesite_none:
-                    kwargs["samesite"] = "None"
-                    response.set_cookie(key, **kwargs)
-                else:
-                    response.set_cookie(key, **kwargs)
-                    response.cookies[key]["samesite"] = "None"
-
-                logger.info(f"update_response: cookies[{key}] Partitioned=True")
                 response.cookies[key]["Partitioned"] = True
+                logger.info(f"BLTICookieService: {key}: set Partitioned")
             else:
-                logger.info(f"update_response: non secure cooke {key}")
-                response.set_cookie(key, **kwargs)
-
-
+                logger.info(f"BLTICookieService: {key}: insecure request")
