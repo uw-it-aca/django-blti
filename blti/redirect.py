dab97fc66763f61f74d079b5210ae6abe48d54fd
mike seibel <mikes@washington.edu>
2025-03-20 07:48:16 -0700

    debug

--- blti/redirect.py
+++ blti/redirect.py
@@ -14,6 +14,7 @@ class BLTIRedirect(DjangoRedirect):
             HttpResponse(
                 f"""\
                 <script type="text/javascript">
+                debugger
                 const redirect_location="{self._location}",
                 """
                 """
