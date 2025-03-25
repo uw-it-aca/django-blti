345e66d353df7df49e88820d729a9429f2763f66
mike seibel <mikes@washington.edu>
2025-03-25 15:04:32 -0700

    proper superclass

--- blti/request.py
+++ blti/request.py
@@ -1,10 +1,10 @@
 # Copyright 2025 UW-IT, University of Washington
 # SPDX-License-Identifier: Apache-2.0
 
-from pylti1p3.request import Request
+from pylti1p3.contrib.django.request import DjangoRequest
 
 
-class BLTIRequest(Request):
+class BLTIRequest(DjangoRequest):
     def __init__(self, request, post_only=False, default_params=None):
         super().__init__(request, post_only, default_params)
 
