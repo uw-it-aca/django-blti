584373d0f2bdef568d2b0d3d2733fc1efef99ff1
mike seibel <mikes@washington.edu>
2025-03-27 08:18:51 -0700

    indent

--- blti/launch_redirect.py
+++ blti/launch_redirect.py
@@ -20,115 +20,115 @@ class BLTILaunchRedirect(DjangoRedirect):
 
     def _js_script(self):
         s = f"""\
-                <script type="text/javascript">
-                const redirect_location = "{self._location}",
-                      redirect_origin = "{self._auth_origin}",
-                      state = "{self._state}",
-                      parsed_redirect = URL.parse(redirect_location),
-                      parsed_params = parsed_redirect.searchParams;
-                """
-                """
-                var client_data = {
-                        nonce: {
-                            value: null
-                        },
-                        state: {
-                            value: state
-                        },
-                        session_cookie_name: {
-                            value: null
-                        },
-                        session_cookie_value: {
-                            value: null
-                        }
-                    };
-
-debugger
-                function doRedirection() {
-debugger
-                    window.location=redirect_location;
-                }
-
-                function validClientData() {
-                    for (const prop in client_data) {
-                        if (!client_data[prop].value) {
-                            console.log("incomplete client data: " + prop);
-                            return false;
-                        }
+            <script type="text/javascript">
+            const redirect_location = "{self._location}",
+                  redirect_origin = "{self._auth_origin}",
+                  state = "{self._state}",
+                  parsed_redirect = URL.parse(redirect_location),
+                  parsed_params = parsed_redirect.searchParams;
+            """
+            """
+            var client_data = {
+                    nonce: {
+                        value: null
+                    },
+                    state: {
+                        value: state
+                    },
+                    session_cookie_name: {
+                        value: null
+                    },
+                    session_cookie_value: {
+                        value: null
                     }
+                };
 
-                    return true;
-                }
+            debugger
+            function doRedirection() {
+            debugger
+                window.location=redirect_location;
+            }
 
-                function clientDataMessageId(prop) {
-                    return prop + '_' + state;
-                }
-
-                function getClientData(frame) {
-                    for (const prop in client_data) {
-                        ltiClientStore(frame, {
-                            subject: 'lti.get_data',
-                            message_id: clientDataMessageId(prop),
-                            key: prop
-                        });
+            function validClientData() {
+                for (const prop in client_data) {
+                    if (!client_data[prop].value) {
+                        console.log("incomplete client data: " + prop);
+                        return false;
                     }
                 }
 
-                function dataFetched(frame) {
-                    for (const prop in client_data) {
-                        if (!client_data[prop].value) {
-                            return false;
-                        }
+                return true;
+            }
+
+            function clientDataMessageId(prop) {
+                return prop + '_' + state;
+            }
+
+            function getClientData(frame) {
+                for (const prop in client_data) {
+                    ltiClientStore(frame, {
+                        subject: 'lti.get_data',
+                        message_id: clientDataMessageId(prop),
+                        key: prop
+                    });
+                }
+            }
+
+            function dataFetched(frame) {
+                for (const prop in client_data) {
+                    if (!client_data[prop].value) {
+                        return false;
                     }
-                    return true;
                 }
+                return true;
+            }
 
-                function ltiClientStore(frame, data) {
-                    console.log("lti.get_data (" + redirect_origin + "): ", data);
-                    window.parent.frames[frame].postMessage(data, redirect_origin);
-                }
+            function ltiClientStore(frame, data) {
+                console.log("lti.get_data (" + redirect_origin + "): ", data);
+                window.parent.frames[frame].postMessage(data, redirect_origin);
+            }
 
-                function ltiClientStoreResponse(event) {
-                    var message = event.data;
-                    switch (message.subject) {
-                        case 'lti.capabilities.response':
-                            var supported = message.supported_messages;
-                            for (var i = 0; i < supported.length; i++) {
-                                if (supported[i].subject == "lti.get_data") {
-                                    getClientData(supported[i].frame);
-                                }
+            function ltiClientStoreResponse(event) {
+                var message = event.data;
+                switch (message.subject) {
+                    case 'lti.capabilities.response':
+                        var supported = message.supported_messages;
+                        for (var i = 0; i < supported.length; i++) {
+                            if (supported[i].subject == "lti.get_data") {
+                                getClientData(supported[i].frame);
                             }
-                        break;
-                        case 'lti.get_data.response':
+                        }
+                    break;
+                    case 'lti.get_data.response':
 
 
-                            console.log(message.subject +
-                                        ": key=" + message.key +
-                                        ", value=" + message.value);
+                        console.log(message.subject +
+                                    ": key=" + message.key +
+                                    ", value=" + message.value);
 
-                            client_data[message.key].value = message.value;
-                            if (dataFetched()) {
-                                doRedirection();
-                            }
-                        break;
-                    }
-
-                    if (message.error) {
-                        console.error("event " + message.subject + 
-                                      " error (" + message.error.code +
-                                      "): " + message.error.message);
-                    }
+                        client_data[message.key].value = message.value;
+                        if (dataFetched()) {
+                            doRedirection();
+                        }
+                    break;
                 }
 
-                function clientStoreAndRedirect() {
-                    window.parent.postMessage({subject: 'lti.capabilities'}, '*');
-                    setTimeout(doRedirection, 60000);
+                if (message.error) {
+                    console.error("event " + message.subject + 
+                                  " error (" + message.error.code +
+                                  "): " + message.error.message);
                 }
+            }
 
-                window.addEventListener("message", ltiClientStoreResponse);
-                document.addEventListener("DOMContentLoaded", clientStoreAndRedirect);
-                </script>
-                """
+            function clientStoreAndRedirect() {
+                window.parent.postMessage({subject: 'lti.capabilities'}, '*');
+                setTimeout(doRedirection, 60000);
+            }
+
+            window.addEventListener("message", ltiClientStoreResponse);
+            document.addEventListener("DOMContentLoaded", clientStoreAndRedirect);
+            </script>
+            """
         logger.info(f"js_script: {s}")
         return s
 
