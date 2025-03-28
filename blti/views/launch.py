5bbe0184c6866e19cfb0520d4ec2704a4930c7f8
mike seibel <mikes@washington.edu>
2025-03-28 16:57:09 -0700

    client side storage redirects

--- blti/views/launch.py
+++ blti/views/launch.py
@@ -8,10 +8,16 @@ from django.views.decorators.csrf import csrf_exempt
 from blti.config import get_tool_conf, get_launch_data_storage
 from blti.exceptions import BLTIException
 from blti.validators import BLTIRequestValidator
+from blti.launch_redirect import BLTILaunchRedirect
 from pylti1p3.exception import OIDCException
 from pylti1p3.contrib.django import DjangoMessageLaunch
 from oauthlib.oauth1.rfc5849.endpoints.signature_only import (
     SignatureOnlyEndpoint)
+from pylti1p3.contrib.django.request import DjangoRequest
+from pylti1p3.contrib.django.cookie import DjangoCookieService
+from pylti1p3.contrib.django.session import DjangoSessionService
+from urllib.parse import urljoin, urlparse, urlencode
+import json
 import logging
 
 
@@ -38,6 +44,39 @@ class BLTILaunchView(BLTIView):
             logger.debug(f"LTI 1.1 launch")
         except BLTIException as ex:
             try:
+                # if client storage indicated, redirect to collect cookies
+                django_request = DjangoRequest(request)
+                cookie_serice = DjangoCookieService(django_request)
+                session_service = DjangoSessionService(request)
+                data_storage = get_launch_data_storage()
+                data_storage.set_request(django_request)
+
+                session_cookie_name = data_storage._prepare_key(
+                    data_storage.get_session_cookie_name())
+                session_id = cookie_serice.get_cookie(session_cookie_name)
+                if not session_id:
+                    # peel params inserted from client side storage
+                    # off and insert them into the request validation
+                    session_id = self.get_parameter(
+                        request, 'lti1p3_session_id')
+                    if session_id:
+                        # insert session cookie
+                        request.COOKIES[session_cookie_name] = session_id
+
+                        # insert state cookie
+                        state = self.get_parameter(request, 'lti1p3_state')
+                        request.COOKIES[f"lti1p3-{state}"] = state
+
+                        # add nonce to session
+                        nonce = self.get_parameter(request, 'lti1p3_nonce')
+                        session_service.save_nonce(nonce)
+
+                        # fall thru to 1.3 launch
+
+                    elif self.get_parameter(request, 'lti_storage_target'):
+                        logger.debug(f"LTI 1.3 client side store redirect")
+                        return self.client_store_redirect(request)
+
                 launch_data = self.validate_1p3(request)
                 logger.debug(f"LTI 1.3 launch")
             except OIDCException as ex:
@@ -53,14 +92,6 @@ class BLTILaunchView(BLTIView):
 
         self.set_session(**launch_data)
         return super(BLTILaunchView, self).dispatch(request, *args, **kwargs)
-
-    def validate_1p3(self, request):
-        tool_conf = get_tool_conf()
-        launch_data_storage = get_launch_data_storage()
-        message_launch = DjangoMessageLaunch(
-            request, tool_conf, launch_data_storage=launch_data_storage)
-        message_launch_data = message_launch.get_launch_data()
-        return message_launch_data
 
     def validate_1p1(self, request):
         request_validator = BLTIRequestValidator()
@@ -82,3 +113,49 @@ class BLTILaunchView(BLTIView):
 
         blti_params = dict(oauth_req.params)
         return blti_params
+
+    def validate_1p3(self, request):
+        tool_conf = get_tool_conf()
+        launch_data_storage = get_launch_data_storage()
+        message_launch = DjangoMessageLaunch(
+            request, tool_conf, launch_data_storage=launch_data_storage)
+        message_launch_data = message_launch.get_launch_data()
+        return message_launch_data
+
+    def client_store_redirect(self, request):
+        redirect_uri = request.build_absolute_uri()
+        params = {
+            'state': self.get_parameter(
+                request, 'state'),
+            'authenticity_token': self.get_parameter(
+                request, 'authenticity_token'),
+            'id_token': self.get_parameter(
+                request, 'id_token'),
+            'utf8': self.get_parameter(
+                request, 'utf8')
+            }
+
+        if redirect_uri.startswith('http:') and request.is_secure():
+            redirect_uri = f"https{uri[4:]}"
+
+        # dig iss out of jwt to get oidc auth origin
+        iss = self.sniff_at_jwt('iss', params['id_token'])
+        reg = get_tool_conf().find_registration_by_issuer(iss)
+        auth_url = urlparse(reg.get_auth_token_url())
+        auth_origin = f"{auth_url.scheme}://{auth_url.netloc}"
+
+        return BLTILaunchRedirect(
+            redirect_uri, params, auth_origin).do_js_redirect()
+
+    def sniff_at_jwt(self, key, id_token):
+        parts = id_token.split('.')
+        if len(parts) != 3:
+            raise BLTIException("Invalid id_token")
+
+        raw_body = DjangoMessageLaunch.urlsafe_b64decode(parts[1])
+        body = json.loads(raw_body)
+        return body[key] if key in body else None
+
+    def get_parameter(self, request, key):
+        params = request.POST if request.method == 'POST' else request.GET
+        return params.get(key)
