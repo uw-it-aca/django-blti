b10a7c3ab7ff12ede4e6b14d5b064a1b9a38485e
mike seibel <mikes@washington.edu>
2025-03-31 07:57:01 -0700

    misplaced arg

--- blti/tests/test_views.py
+++ blti/tests/test_views.py
@@ -12,12 +12,12 @@ class TestLaunchViews(TestCase):
         self.client = Client()
 
     def test_dev_launch_view(self):
-        response = self.client.get(reverse('dev-launch', secure=True), {
+        response = self.client.get(reverse('dev-launch'), {
             'role': 'Instructor',
             'campus': 'seattle',
-        })
+        }, secure=True)
 
         self.assertEqual(response.status_code, 200)
 
-        response = self.client.post(reverse('lti-launch', secure=True))
+        response = self.client.post(reverse('lti-launch'), secure=True)
         self.assertEqual(response.status_code, 200)
